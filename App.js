c26c891ac6d8d019b3794c8212f519337024492a
Wilian Pereira <wilian_pereira92@outlook.com>
2022-03-22 16:59:43 -0300

    feat: Adicionando o componente TopBar

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import TopBar from './componets/TopBar';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <TopBar />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
