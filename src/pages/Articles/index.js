3aae8bf440439ddac2cef3d059d452047a1530e2
Wilian Pereira <wilian_pereira92@outlook.com>
2022-03-24 01:00:31 -0300

    feat: finalizando o componente Articles

--- src/pages/Articles/index.js
+++ src/pages/Articles/index.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, View, Image } from 'react-native';
+import { StyleSheet,Text, View, Image, ScrollView, useWindowDimensions } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import RenderHTML from 'react-native-render-html';
+import Footer from '../../componets/Footer';
 
 function Articles() {
+  const route = useRoute();
+  const content = route.params?.content;
+  const title = route.params?.title;
+  const { width } = useWindowDimensions();
   return (
-    <View>
-      <Text>Artigos</Text>
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={styles.content}>
+          <Text style={styles.title}>{title}</Text>
+          <RenderHTML  tagsStyles={{img: {width: 310, height: 150}}} contentWidth={width} source={ {html: content} } />
+        </View>
+        <Footer style={styles.footer}/>
+      </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignContent: 'center',
+    width: '100%',
+    backgroundColor: '#FFF'
+  },
+  title: {
+    fontSize: 30,
+    width: 300,
+    color: '#1abc9c',
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center'
+  },
+  content: {
+    width: '100%',
+    padding: 50,
+  },
+});
 
 export default Articles;
