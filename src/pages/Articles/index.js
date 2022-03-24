68fa8b82648b709db9e78070086a83a5bae97a27
Wilian Pereira <wilian_pereira92@outlook.com>
2022-03-23 22:32:03 -0300

    feat: implementando a page Articles

---
+++ src/pages/Articles/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, SafeAreaView, View, Image } from 'react-native';
+
+function Articles() {
+  return (
+    <View>
+      <Text>Artigos</Text>
+    </View>
+  );
+}
+
+export default Articles;
