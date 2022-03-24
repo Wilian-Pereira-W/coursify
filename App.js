487490a0bbf4fbf7c6464a2c515c5ab4c1bde0cc
Wilian Pereira <wilian_pereira92@outlook.com>
2022-03-24 00:59:53 -0300

    feat: implementando Stack Screen do Articles

--- App.js
+++ App.js
@@ -29,25 +29,30 @@ function App() {
             )
           }}
         />
-        <Stack.Screen name="Articles" component={Articles} />
+        <Stack.Screen 
+          name="Articles" 
+          options={{
+            headerBackVisible:"false",
+            headerTitle: (props) => (
+              <>
+                <Image
+                  source={require('./src/assets/images/logo-2.png')}
+                />
+                <View style={styles.menu}>
+                <Image style={styles.menuImg}
+                  source={require('./src/assets/images/menu.png')}
+                />
+                </View>
+              </>
+            )
+          }}
+          component={Articles} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flexDirection: 'row',
-  //   alignItems: 'center',
-  //   justifyContent: 'space-around',
-  //   width: '100%',
-  //   marginTop: 35,
-  //   height: 60,
-  //   borderBottomWidth: 1,
-  //   borderColor: '#D3D3D3',
-  //   paddingLeft: 10,
-  //   paddingRight: 10,
-  // },
   menu: {
     backgroundColor: '#1abc9c',
     borderRadius: 50,
