fa7d495c9294a2a4a69498465070c2fdc310cfb6
Wilian Pereira <wilian_pereira92@outlook.com>
2022-03-23 22:29:57 -0300

    feat:configurando app com as rotas

--- App.js
+++ App.js
@@ -1,38 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import Header from './src/componets/Header';
-import Categories from './src/componets/Categories';
-import api from './src/services/api';
+import React from 'react';
+import { Image, View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/pages/Home';
+import Articles from './src/pages/Articles';
 
-export default function App() {
-  const [categories, setCategories] = useState([]);
+const Stack = createNativeStackNavigator();
 
-  useEffect(() => {
-    api
-      .get("categories/")
-      .then((response) => setCategories(response.data))
-      .catch((err) => {
-        console.error("ops! ocorreu um erro" + err);
-      });
-  }, []);
-
+function App() {
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <Header />
-        <Categories dataCategories={categories} />
-        <StatusBar style="auto" />
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Home" 
+          component={Home} 
+          options={{
+            headerTitle: (props) => (
+              <>
+                <Image
+                  source={require('./src/assets/images/logo-2.png')}
+                />
+                <View style={styles.menu}>
+                <Image style={styles.menuImg}
+                  source={require('./src/assets/images/menu.png')}
+                />
+                </View>
+              </>
+            )
+          }}
+        />
+        <Stack.Screen name="Articles" component={Articles} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#FFF',
+  // container: {
+  //   flexDirection: 'row',
+  //   alignItems: 'center',
+  //   justifyContent: 'space-around',
+  //   width: '100%',
+  //   marginTop: 35,
+  //   height: 60,
+  //   borderBottomWidth: 1,
+  //   borderColor: '#D3D3D3',
+  //   paddingLeft: 10,
+  //   paddingRight: 10,
+  // },
+  menu: {
+    backgroundColor: '#1abc9c',
+    borderRadius: 50,
+    width: 40,
+    height: 40,
     alignItems: 'center',
     justifyContent: 'center',
-    height: '100%',
+    marginLeft: 175,
   },
+  menuImg: {
+    width: 30,
+    height: 30,
+  }
 });
+
+export default App;
